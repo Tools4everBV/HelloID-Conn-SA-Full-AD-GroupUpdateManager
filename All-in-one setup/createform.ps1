f8ec4d2f081cb97b5ac53cc1e87664d0279e5341
Michiel van der Veeken <M.vanderVeeken@tools4ever.com>
2020-09-07 15:32:49 +0200

    initial file

---
+++ All-in-one setup/createform.ps1
@@ -0,0 +1,547 @@
+#HelloID variables
+$PortalBaseUrl = "https://CUSTOMER.helloid.com"
+$apiKey = "API_KEY"
+$apiSecret = "API_SECRET"
+$delegatedFormAccessGroupName = "Users"
+ 
+# Create authorization headers with HelloID API key
+$pair = "$apiKey" + ":" + "$apiSecret"
+$bytes = [System.Text.Encoding]::ASCII.GetBytes($pair)
+$base64 = [System.Convert]::ToBase64String($bytes)
+$key = "Basic $base64"
+$headers = @{"authorization" = $Key}
+# Define specific endpoint URI
+if($PortalBaseUrl.EndsWith("/") -eq $false){
+    $PortalBaseUrl = $PortalBaseUrl + "/"
+}
+ 
+ 
+ 
+$variableName = "ADusersSearchOU"
+$variableGuid = ""
+  
+try {
+    $uri = ($PortalBaseUrl +"api/v1/automation/variables/named/$variableName")
+    $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false
+  
+    if([string]::IsNullOrEmpty($response.automationVariableGuid)) {
+        #Create Variable
+        $body = @{
+            name = "$variableName";
+            value = '[{ "OU": "OU=Employees,OU=Users,OU=Enyoi,DC=enyoi-media,DC=local"},{ "OU": "OU=Disabled,OU=Users,OU=Enyoi,DC=enyoi-media,DC=local"},{"OU": "OU=External,OU=Users,OU=Enyoi,DC=enyoi-media,DC=local"}]';
+            secret = "false";
+            ItemType = 0;
+        }
+  
+        $body = $body | ConvertTo-Json
+  
+        $uri = ($PortalBaseUrl +"api/v1/automation/variable")
+        $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false -Body $body
+        $variableGuid = $response.automationVariableGuid
+    } else {
+        $variableGuid = $response.automationVariableGuid
+    }
+  
+    $variableGuid
+} catch {
+    $_
+}
+  
+  
+  
+$variableName = "ADgroupsSearchOU"
+$variableGuid = ""
+  
+try {
+    $uri = ($PortalBaseUrl +"api/v1/automation/variables/named/$variableName")
+    $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false
+  
+    if([string]::IsNullOrEmpty($response.automationVariableGuid)) {
+        #Create Variable
+        $body = @{
+            name = "$variableName";
+            value = '[{ "OU": "OU=Groups,OU=Enyoi,DC=enyoi-media,DC=local"}]';
+            secret = "false";
+            ItemType = 0;
+        }
+  
+        $body = $body | ConvertTo-Json
+  
+        $uri = ($PortalBaseUrl +"api/v1/automation/variable")
+        $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false -Body $body
+        $variableGuid = $response.automationVariableGuid
+    } else {
+        $variableGuid = $response.automationVariableGuid
+    }
+  
+    $variableGuid
+} catch {
+    $_
+}
+  
+  
+  
+  
+$taskName = "AD-group-generate-table-wildcard"
+$taskGetADGroupsGuid = ""
+  
+try {
+    $uri = ($PortalBaseUrl +"api/v1/automationtasks?search=$taskName&container=1")
+    $response = (Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false) | Where-Object -filter {$_.name -eq $taskName}
+  
+    if([string]::IsNullOrEmpty($response.automationTaskGuid)) {
+        #Create Task
+  
+        $body = @{
+            name = "$taskName";
+            useTemplate = "false";
+            powerShellScript = @'
+try {
+    $searchValue = $formInput.searchValue
+    $searchQuery = "*$searchValue*"
+     
+     
+    if([String]::IsNullOrEmpty($searchValue) -eq $true){
+        Hid-Add-TaskResult -ResultValue []
+    }else{
+        Hid-Write-Status -Message "SearchQuery: $searchQuery" -Event Information
+        HID-Write-Summary -Message "Searching for: $searchQuery" -Event Information
+        Hid-Write-Status -Message "SearchBase: $searchOUs" -Event Information
+         
+        $ous = $searchOUs | ConvertFrom-Json
+     
+        $groups = foreach($item in $ous) {
+             Get-ADGroup -Filter {Name -like $searchQuery} -SearchBase $item.ou -properties *
+        }
+         
+        $groups = $groups | Sort-Object -Property Name
+        $resultCount = @($groups).Count
+        Hid-Write-Status -Message "Result count: $resultCount" -Event Information
+        HID-Write-Summary -Message "Result count: $resultCount" -Event Information
+         
+        if(@($groups).Count -gt 0){
+            foreach($group in $groups)
+            {
+                $returnObject = @{name=$group.name; description=$group.description;}
+                Hid-Add-TaskResult -ResultValue $returnObject
+            }
+        }else{
+            Hid-Add-TaskResult -ResultValue []
+        }
+    }
+} catch {
+    HID-Write-Status -Message "Error searching AD user [$searchValue]. Error: $($_.Exception.Message)" -Event Error
+    HID-Write-Summary -Message "Error searching AD user [$searchValue]" -Event Failed
+     
+    Hid-Add-TaskResult -ResultValue []
+}
+'@;
+            automationContainer = "1";
+            variables = @(@{name = "searchOUs"; value = "{{variable.ADgroupsSearchOU}}"; typeConstraint = "string"; secret = "False"})
+        }
+        $body = $body | ConvertTo-Json
+  
+        $uri = ($PortalBaseUrl +"api/v1/automationtasks/powershell")
+        $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false -Body $body
+        $taskGetADGroupsGuid = $response.automationTaskGuid
+  
+    } else {
+        #Get TaskGUID
+        $taskGetADGroupsGuid = $response.automationTaskGuid
+    }
+} catch {
+    $_
+}
+  
+$taskGetADGroupsGuid
+  
+  
+  
+$dataSourceName = "AD-group-generate-table-wildcard"
+$dataSourceGetADGroupsGuid = ""
+  
+try {
+    $uri = ($PortalBaseUrl +"api/v1/datasource/named/$dataSourceName")
+    $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false
+  
+    if([string]::IsNullOrEmpty($response.dataSourceGUID)) {
+        #Create DataSource
+        $body = @{
+            name = "$dataSourceName";
+            type = "3";
+            model = @(@{key = "description"; type = 0}, @{key = "name"; type = 0});
+            automationTaskGUID = "$taskGetADGroupsGuid";
+            input = @(@{description = ""; translateDescription = "False"; inputFieldType = "1"; key = "searchValue"; type = "0"; options = "1"})
+        }
+        $body = $body | ConvertTo-Json
+  
+        $uri = ($PortalBaseUrl +"api/v1/datasource")
+        $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false -Body $body
+          
+        $dataSourceGetADGroupsGuid = $response.dataSourceGUID
+    } else {
+        #Get DatasourceGUID
+        $dataSourceGetADGroupsGuid = $response.dataSourceGUID
+    }
+} catch {
+    $_
+}
+  
+$dataSourceGetADGroupsGuid
+  
+ 
+  
+ $taskName = "AD-user-generate-table"
+$taskGetUsersGuid = ""
+  
+try {
+    $uri = ($PortalBaseUrl +"api/v1/automationtasks?search=$taskName&container=1")
+    $response = (Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false) | Where-Object -filter {$_.name -eq $taskName}
+  
+    if([string]::IsNullOrEmpty($response.automationTaskGuid)) {
+        #Create Task
+  
+        $body = @{
+            name = "$taskName";
+            useTemplate = "false";
+            powerShellScript = @'
+try {
+    $ous = $searchOUs | ConvertFrom-Json
+ 
+    $users = foreach($item in $ous) {
+        Get-ADUser -Filter {Name -like "*"} -SearchBase $item.ou -properties *
+    }
+     
+    $users = $users | Sort-Object -Property DisplayName
+    $resultCount = @($users).Count
+    Hid-Write-Status -Message "Result count: $resultCount" -Event Information
+    HID-Write-Summary -Message "Result count: $resultCount" -Event Information
+     
+    if($resultCount -gt 0){
+        foreach($user in $users){
+            $returnObject = @{SamAccountName=$user.SamAccountName; displayName=$user.displayName; UserPrincipalName=$user.UserPrincipalName; Description=$user.Description; Department=$user.Department; Title=$user.Title;}
+            Hid-Add-TaskResult -ResultValue $returnObject
+        }
+    } else {
+        Hid-Add-TaskResult -ResultValue []
+    }
+} catch {
+    HID-Write-Status -Message "Error searching AD users. Error: $($_.Exception.Message)" -Event Error
+    HID-Write-Summary -Message "Error searching AD users" -Event Failed
+     
+    Hid-Add-TaskResult -ResultValue []
+}
+'@;
+            automationContainer = "1";
+            variables = @(@{name = "searchOUs"; value = "{{variable.ADusersSearchOU}}"; typeConstraint = "string"; secret = "False"})
+        }
+        $body = $body | ConvertTo-Json
+  
+        $uri = ($PortalBaseUrl +"api/v1/automationtasks/powershell")
+        $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false -Body $body
+        $taskGetUsersGuid = $response.automationTaskGuid
+  
+    } else {
+        #Get TaskGUID
+        $taskGetUsersGuid = $response.automationTaskGuid
+    }
+} catch {
+    $_
+}
+  
+$taskGetUsersGuid
+  
+  
+  
+$dataSourceName = "AD-user-generate-table"
+$dataSourceGetUsersGuid = ""
+  
+try {
+    $uri = ($PortalBaseUrl +"api/v1/datasource/named/$dataSourceName")
+    $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false
+  
+    if([string]::IsNullOrEmpty($response.dataSourceGUID)) {
+        #Create DataSource
+        $body = @{
+            name = "$dataSourceName";
+            type = "3";
+            model = @(@{key = "Department"; type = 0}, @{key = "Description"; type = 0}, @{key = "displayName"; type = 0}, @{key = "SamAccountName"; type = 0}, @{key = "Title"; type = 0}, @{key = "UserPrincipalName"; type = 0});
+            automationTaskGUID = "$taskGetUsersGuid";
+            input = @()
+        }
+        $body = $body | ConvertTo-Json
+  
+        $uri = ($PortalBaseUrl +"api/v1/datasource")
+        $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false -Body $body
+          
+        $dataSourceGetUsersGuid = $response.dataSourceGUID
+    } else {
+        #Get DatasourceGUID
+        $dataSourceGetUsersGuid = $response.dataSourceGUID
+    }
+} catch {}
+$dataSourceGetUsersGuid
+  
+  
+  
+$formName = "AD Group - Update manager"
+$formGuid = ""
+  
+try
+{
+    try {
+        $uri = ($PortalBaseUrl +"api/v1/forms/$formName")
+        $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false
+    } catch {
+        $response = $null
+    }
+  
+    if(([string]::IsNullOrEmpty($response.dynamicFormGUID)) -or ($response.isUpdated -eq $true))
+    {
+        #Create Dynamic form
+        $form = @"
+[
+  {
+    "label": "Select group",
+    "fields": [
+      {
+        "key": "searchfield",
+        "templateOptions": {
+          "label": "Search",
+          "placeholder": ""
+        },
+        "type": "input",
+        "summaryVisibility": "Hide element",
+        "requiresTemplateOptions": true
+      },
+      {
+        "key": "gridGroups",
+        "templateOptions": {
+          "label": "Select group",
+          "required": true,
+          "grid": {
+            "columns": [
+              {
+                "headerName": "Name",
+                "field": "name"
+              },
+              {
+                "headerName": "Description",
+                "field": "description"
+              }
+            ],
+            "height": 300,
+            "rowSelection": "single"
+          },
+          "dataSourceConfig": {
+            "dataSourceGuid": "$dataSourceGetADGroupsGuid",
+            "input": {
+              "propertyInputs": [
+                {
+                  "propertyName": "searchValue",
+                  "otherFieldValue": {
+                    "otherFieldKey": "searchfield"
+                  }
+                }
+              ]
+            }
+          },
+          "useFilter": false
+        },
+        "type": "grid",
+        "summaryVisibility": "Show",
+        "requiresTemplateOptions": true
+      }
+    ]
+  },
+  {
+    "label": "Manager",
+    "fields": [
+      {
+        "key": "managedBy",
+        "templateOptions": {
+          "label": "AD account manager",
+          "required": true,
+          "grid": {
+            "columns": [
+              {
+                "headerName": "DisplayName",
+                "field": "displayName"
+              },
+              {
+                "headerName": "UserPrincipalName",
+                "field": "UserPrincipalName"
+              },
+              {
+                "headerName": "Department",
+                "field": "Department"
+              },
+              {
+                "headerName": "Title",
+                "field": "Title"
+              },
+              {
+                "headerName": "Description",
+                "field": "Description"
+              }
+            ],
+            "height": 300,
+            "rowSelection": "single"
+          },
+          "dataSourceConfig": {
+            "dataSourceGuid": "$dataSourceGetUsersGuid",
+            "input": {
+              "propertyInputs": []
+            }
+          },
+          "useFilter": true
+        },
+        "type": "grid",
+        "summaryVisibility": "Show",
+        "requiresTemplateOptions": true
+      }
+    ]
+  }
+]
+"@
+  
+        $body = @{
+            Name = "$formName";
+            FormSchema = $form
+        }
+        $body = $body | ConvertTo-Json
+  
+        $uri = ($PortalBaseUrl +"api/v1/forms")
+        $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false -Body $body
+  
+        $formGuid = $response.dynamicFormGUID
+    } else {
+        $formGuid = $response.dynamicFormGUID
+    }
+} catch {
+    $_
+}
+  
+$formGuid
+  
+  
+  
+  
+$delegatedFormAccessGroupGuid = ""
+  
+try {
+    $uri = ($PortalBaseUrl +"api/v1/groups/$delegatedFormAccessGroupName")
+    $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false
+    $delegatedFormAccessGroupGuid = $response.groupGuid
+} catch {
+    $_
+}
+  
+$delegatedFormAccessGroupGuid
+  
+  
+  
+$delegatedFormName = "AD Groep - Update manager"
+$delegatedFormGuid = ""
+  
+try {
+    try {
+        $uri = ($PortalBaseUrl +"api/v1/delegatedforms/$delegatedFormName")
+        $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false
+    } catch {
+        $response = $null
+    }
+  
+    if([string]::IsNullOrEmpty($response.delegatedFormGUID)) {
+        #Create DelegatedForm
+        $body = @{
+            name = "$delegatedFormName";
+            dynamicFormGUID = "$formGuid";
+            isEnabled = "True";
+            accessGroups = @("$delegatedFormAccessGroupGuid");
+            useFaIcon = "True";
+            faIcon = "fa fa-user-secret";
+        }  
+  
+        $body = $body | ConvertTo-Json
+  
+        $uri = ($PortalBaseUrl +"api/v1/delegatedforms")
+        $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false -Body $body
+  
+        $delegatedFormGuid = $response.delegatedFormGUID
+    } else {
+        #Get delegatedFormGUID
+        $delegatedFormGuid = $response.delegatedFormGUID
+    }
+} catch {
+    $_
+}
+  
+$delegatedFormGuid
+  
+  
+  
+  
+$taskActionName = "AD-group-set-managedby"
+$taskActionGuid = ""
+  
+try {
+    $uri = ($PortalBaseUrl +"api/v1/automationtasks?search=$taskActionName&container=8")
+    $response = Invoke-RestMethod -Method Get -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false
+  
+    if([string]::IsNullOrEmpty($response.automationTaskGuid)) {
+        #Create Task
+  
+        $body = @{
+            name = "$taskActionName";
+            useTemplate = "false";
+            powerShellScript = @'
+try {
+    $adGroup = Get-ADgroup -Filter { Name -eq $groupName }
+    HID-Write-Status -Message "Found AD group [$groupName]" -Event Information
+    HID-Write-Summary -Message "Found AD group [$groupName]" -Event Information
+} catch {
+    HID-Write-Status -Message "Could not find AD group [$groupName]. Error: $($_.Exception.Message)" -Event Error
+    HID-Write-Summary -Message "Failed to find AD group [$groupName]" -Event Failed
+}
+ 
+try {
+    $managerObject = Get-ADuser -Filter { UserPrincipalName -eq $managerUPN }
+    HID-Write-Status -Message "Found AD user [$managerUPN]" -Event Information
+    HID-Write-Summary -Message "Found AD user [$managerUPN]" -Event Information
+} catch {
+    HID-Write-Status -Message "Could not find AD user [$managerUPN]. Error: $($_.Exception.Message)" -Event Error
+    HID-Write-Summary -Message "Failed to find AD user [$managerUPN]" -Event Failed
+}
+ 
+ 
+try {
+    Set-ADGroup -identity $adGroup -managedBy $managerObject
+     
+    HID-Write-Status -Message "Finished updated ManagedBy of [$groupName] to [$managerUPN]" -Event Success
+    HID-Write-Summary -Message "Successfully updated ManagedBy of [$groupName] to [$managerUPN]" -Event Success
+} catch {
+    HID-Write-Status -Message "Could not update ManagedBy of [$groupName] to [$managerUPN]. Error: $($_.Exception.Message)" -Event Error
+    HID-Write-Summary -Message "Failed to update ManagedBy of [$groupName] to [$managerUPN]" -Event Failed
+}
+'@;
+            automationContainer = "8";
+            objectGuid = "$delegatedFormGuid";
+            variables = @(@{name = "managerUPN"; value = "{{form.managedBy.UserPrincipalName}}"; typeConstraint = "string"; secret = "False"},
+                        @{name = "groupName"; value = "{{form.gridGroups.name}}"; typeConstraint = "string"; secret = "False"});
+        }
+        $body = $body | ConvertTo-Json
+  
+        $uri = ($PortalBaseUrl +"api/v1/automationtasks/powershell")
+        $response = Invoke-RestMethod -Method Post -Uri $uri -Headers $headers -ContentType "application/json" -Verbose:$false -Body $body
+        $taskActionGuid = $response.automationTaskGuid
+  
+    } else {
+        #Get TaskGUID
+        $taskActionGuid = $response.automationTaskGuid
+    }
+} catch {
+    $_
+}
+  
+$taskActionGuid
